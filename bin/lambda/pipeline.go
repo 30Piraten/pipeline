60f8989cf01569fa913bbf5858dc6e9f887d24aa
30Piraten <victorehikioyaraeva@outlook.com>
2025-02-21 11:57:09 +0100

    Updated pipeline

--- bin/lambda/pipeline.go
+++ bin/lambda/pipeline.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
+	"context"
 
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
@@ -14,20 +11,9 @@ type Response struct {
 	Message string `json:"message"`
 }
 
-// Handler function for Lambda
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	fmt.Println("Received request:", request.Body)
-
-	response := Response{
-		Message: "Die Rechnung bitte! 🚀",
-	}
-
-	body, _ := json.Marshal(response)
-	return events.APIGatewayProxyResponse{
-		StatusCode: http.StatusOK,
-		Body:       string(body),
-		Headers:    map[string]string{"Content-Type": "application/json"},
-	}, nil
+// Handler function for AWS Lambda
+func handler(ctx context.Context) (string, error) {
+	return "Lambda deployment successful", nil
 }
 
 func main() {
